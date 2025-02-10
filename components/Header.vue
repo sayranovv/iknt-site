75a1b29541ede9e1767077b33d4298a4009ce8c1
Eldar Sayranov <sayranoveldar@gmail.com>
2025-02-10 21:18:54 +0500

    created structure

---
+++ components/Header.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <div>dfdfgdf</div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="mx-auto max-w-2xl bg-black">
+    <Header />
+    <div>
+      <Slot />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
