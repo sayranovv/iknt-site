18f3e0f611ccbe93428dcc257a2bf8dec228b358
Eldar Sayranov <sayranoveldar@gmail.com>
2025-02-21 13:24:07 +0500

    edited slider

--- components/Sliders/EdProgramsSlider.vue
+++ components/Sliders/EdProgramsSlider.vue
@@ -24,7 +24,7 @@ const buttonClick = (tab: string): void => {
     <div class="mx-auto mt-12 rounded-lg bg-white bg-opacity-10 flex w-fit">
       <p
         @click="buttonClick(null)"
-        class="border-t border-b border-l rounded-l-lg px-4 py-2 hover:border-teal-400 hover:shadow-[0px_0px_30px_10px_rgba(0,255,150,0.4)] backdrop-blur-2xl transition-all before:absolute before:inset-0 before:bg-teal-500 before:rounded-l-lg before:opacity-0 hover:before:opacity-20"
+        class="cursor-pointer border-t border-b border-l rounded-l-lg px-4 py-2 hover:border-teal-400 hover:shadow-[0px_0px_30px_10px_rgba(0,255,150,0.4)] backdrop-blur-2xl transition-all before:absolute before:inset-0 before:bg-teal-500 before:rounded-l-lg before:opacity-0 hover:before:opacity-20"
         :class="
           activeTab === null
             ? [
@@ -50,7 +50,7 @@ const buttonClick = (tab: string): void => {
       </p>
       <p
         @click="buttonClick('Бакалавриат')"
-        class="border-t border-b py-2 px-2.5 hover:border-blue-400 hover:shadow-[0px_0px_30px_10px_rgba(0,0,255,0.4)] backdrop-blur-2xl transition-all before:absolute before:inset-0 before:bg-blue-500 before:opacity-0 hover:before:opacity-20"
+        class="cursor-pointer border-t border-b py-2 px-2.5 hover:border-blue-400 hover:shadow-[0px_0px_30px_10px_rgba(0,0,255,0.4)] backdrop-blur-2xl transition-all before:absolute before:inset-0 before:bg-blue-500 before:opacity-0 hover:before:opacity-20"
         :class="
           activeTab === 'Бакалавриат'
             ? [
@@ -76,7 +76,7 @@ const buttonClick = (tab: string): void => {
       </p>
       <p
         @click="buttonClick('Специалитет')"
-        class="border-t border-b py-2 px-2.5 hover:border-fuchsia-400 hover:shadow-[0px_0px_30px_10px_rgba(255,0,255,0.4)] backdrop-blur-2xl transition-all before:absolute before:inset-0 before:bg-fuchsia-500 hover:before:opacity-20"
+        class="cursor-pointer border-t border-b py-2 px-2.5 hover:border-fuchsia-400 hover:shadow-[0px_0px_30px_10px_rgba(255,0,255,0.4)] backdrop-blur-2xl transition-all before:absolute before:inset-0 before:bg-fuchsia-500 hover:before:opacity-20"
         :class="
           activeTab === 'Специалитет'
             ? [
@@ -102,7 +102,7 @@ const buttonClick = (tab: string): void => {
       </p>
       <p
         @click="buttonClick('Магистратура')"
-        class="border-t border-b border-r rounded-r-lg px-4 py-2 hover:border-orange-400 hover:shadow-[0px_0px_30px_10px_rgba(255,150,0,0.4)] backdrop-blur-2xl transition-all before:absolute before:inset-0 before:bg-orange-500 before:rounded-r-lg before:opacity-0 hover:before:opacity-20"
+        class="cursor-pointer border-t border-b border-r rounded-r-lg px-4 py-2 hover:border-orange-400 hover:shadow-[0px_0px_30px_10px_rgba(255,150,0,0.4)] backdrop-blur-2xl transition-all before:absolute before:inset-0 before:bg-orange-500 before:rounded-r-lg before:opacity-0 hover:before:opacity-20"
         :class="
           activeTab === 'Магистратура'
             ? [
