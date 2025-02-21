2755fed16c24e30a1a0f7b256d87aac10d162a5b
Eldar Sayranov <sayranoveldar@gmail.com>
2025-02-21 22:53:19 +0500

    created applicant card component

---
+++ components/Cards/ApplicantCard.vue
@@ -0,0 +1,127 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import VanillaTilt from 'vanilla-tilt'
+
+onMounted(() => {
+  VanillaTilt.init(document.querySelectorAll('.card'), {
+    max: 25,
+    speed: 400,
+    glare: true,
+    'max-glare': 0.5
+  })
+})
+</script>
+
+<template>
+  <div class="container">
+    <div class="card">
+      <div class="imgBx">
+        <div class="bg-blue-900 w-full h-full flex items-end">
+          <span class="text-3xl font-bold ml-2 mb-2">Госуслуги</span>
+        </div>
+      </div>
+      <div class="content">
+        <h2 class="text-xl font-bold">Госуслуги</h2>
+        <p class="my-2">
+          Если заявление подано через Госуслуги, то СНИЛС, Паспорт и аттестат подгрузятся сами
+        </p>
+        <a href="https://www.gosuslugi.ru/vuzonline" class="border-b">Перейти</a>
+      </div>
+    </div>
+    <div class="card">
+      <div class="imgBx">
+        <div class="bg-rose-900 w-full h-full flex items-end">
+          <span class="text-3xl font-bold ml-2 mb-2">ЛК ПГНИУ</span>
+        </div>
+      </div>
+      <div class="content">
+        <h2 class="text-xl font-bold">Личный кабинет ПГНИУ</h2>
+        <p class="my-2">
+          Зарегистрируйтесь в личном кабинете абитуриента и загрузите сканы ваших документов
+        </p>
+        <a href="https://abiturient.psu.ru/etis/abiturient/lock.html" class="border-b">Перейти</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  position: relative;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 50px 50px;
+  transform-style: preserve-3d;
+}
+
+.container .card {
+  width: 300px;
+  height: 260px;
+  margin: 50px 30px;
+  background: rgba(255, 255, 255, 0.1);
+  display: flex;
+  border-radius: 4px;
+  flex-direction: column;
+  transform-style: preserve-3d;
+  box-shadow: 0 5px 20px rgba(0, 0, 0, 0.2);
+  transition: height 0.5s ease-in-out;
+}
+
+.container .card:hover {
+  height: 440px;
+}
+
+.container .card .imgBx {
+  position: relative;
+  top: 0;
+  left: 0;
+  right: 0;
+  min-width: initial;
+  width: 300px;
+  height: 260px;
+  border-radius: 4px;
+  overflow: hidden;
+  z-index: 1;
+  box-shadow: 0 5px 20px rgba(0, 0, 0, 0.2);
+  transform-style: preserve-3d;
+  transition: 0.5s;
+}
+
+.container .card:hover .imgBx {
+  top: -60px;
+  left: 20px;
+  right: 20px;
+  width: 260px;
+  height: 260px;
+  transform: translate3d(0, 0, 75px);
+}
+
+.js-tilt-glare {
+  border-radius: 4px;
+}
+
+.container .card .imgBx div {
+  max-width: 100%;
+  border-radius: 4px;
+}
+
+.container .card .content {
+  position: relative;
+  padding: 0 15px;
+  left: 135px;
+  text-align: center;
+  color: #fff;
+  display: none;
+  transition: 0.5s;
+  transform: translate(-140px) scale(1);
+}
+
+.container .card:hover .content {
+  display: block;
+  opacity: 1;
+  transform: translate(-140px) scale(1);
+}
+</style>
--- components/Steps.vue
+++ components/Steps.vue
@@ -26,7 +26,7 @@ const steps = [
 <template>
   <div class="relative flex justify-between items-start gap-5">
     <div
-      class="absolute top-14 left-1/2 -translate-x-1/2 w-5/6 h-0.5 bg-white bg-opacity-50 z-0"
+      class="absolute top-14 left-1/2 -translate-x-1/2 w-5/6 h-1 bg-white bg-opacity-30 z-0"
     ></div>
     <div
       class="relative flex flex-col gap-4 items-center w-1/4"
@@ -34,7 +34,7 @@ const steps = [
       :key="step.id"
     >
       <div
-        class="w-28 h-28 text-6xl font-black flex justify-center items-center rounded-full relative border border-white border-opacity-200 bg-white bg-opacity-10 backdrop-blur-lg transition-all hover:shadow-[0px_0px_30px_5px_rgba(0,0,255,0.4)] hover:border-blue-400 before:absolute before:inset-0 before:bg-gradient-to-br before:from-transparent before:to-blue-500 before:opacity-0 hover:before:opacity-30 before:rounded-full"
+        class="w-28 h-28 text-6xl font-black flex justify-center items-center rounded-full relative border-4 border-white border-opacity-20 bg-white bg-opacity-10 backdrop-blur-lg transition-all hover:shadow-[0px_0px_50px_20px_rgba(255,150,0,0.4)] hover:border-orange-500 before:absolute before:inset-0 before:bg-gradient-to-br before:from-transparent before:to-orange-500 before:opacity-0 hover:before:opacity-30 before:rounded-full cursor-pointer"
       >
         {{ step.id }}
       </div>
