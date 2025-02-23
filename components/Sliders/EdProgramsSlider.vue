685121822dbd45a06a0089ec7adcc6f5097a5fc2
Eldar Sayranov <sayranoveldar@gmail.com>
2025-02-23 19:03:43 +0500

    created adaptive for comp

--- components/Sliders/EdProgramsSlider.vue
+++ components/Sliders/EdProgramsSlider.vue
@@ -21,10 +21,116 @@ const buttonClick = (tab: string): void => {
 
 <template>
   <div>
-    <div class="mx-auto mt-12 rounded-lg bg-white bg-opacity-10 flex w-fit">
+    <!--    <div class="mx-auto mt-12 rounded-lg bg-white bg-opacity-10 flex w-fit">-->
+    <!--      <p-->
+    <!--        @click="buttonClick(null)"-->
+    <!--        class="cursor-pointer border-t border-b border-l rounded-l-lg px-4 py-2 hover:border-teal-400 hover:shadow-[0px_0px_30px_10px_rgba(0,255,150,0.4)] backdrop-blur-2xl transition-all before:absolute before:inset-0 before:bg-teal-500 before:rounded-l-lg before:opacity-0 hover:before:opacity-20"-->
+    <!--        :class="-->
+    <!--          activeTab === null-->
+    <!--            ? [-->
+    <!--                'z-10',-->
+    <!--                'border-teal-400',-->
+    <!--                'shadow-[0px_0px_30px_10px_rgba(0,255,150,0.4)]',-->
+    <!--                'before:absolute',-->
+    <!--                'before:inset-0',-->
+    <!--                'before:bg-teal-500',-->
+    <!--                'before:opacity-20'-->
+    <!--              ]-->
+    <!--            : [-->
+    <!--                'relative',-->
+    <!--                'z-20',-->
+    <!--                'hover:z-0',-->
+    <!--                'before:opacity-0',-->
+    <!--                'border-white',-->
+    <!--                'border-opacity-20'-->
+    <!--              ]-->
+    <!--        "-->
+    <!--      >-->
+    <!--        Все-->
+    <!--      </p>-->
+    <!--      <p-->
+    <!--        @click="buttonClick('Бакалавриат')"-->
+    <!--        class="cursor-pointer border-t border-b py-2 px-2.5 hover:border-blue-400 hover:shadow-[0px_0px_30px_10px_rgba(0,0,255,0.4)] backdrop-blur-2xl transition-all before:absolute before:inset-0 before:bg-blue-500 before:opacity-0 hover:before:opacity-20"-->
+    <!--        :class="-->
+    <!--          activeTab === 'Бакалавриат'-->
+    <!--            ? [-->
+    <!--                'z-10',-->
+    <!--                'border-blue-400',-->
+    <!--                'shadow-[0px_0px_30px_10px_rgba(0,0,255,0.4)]',-->
+    <!--                'before:absolute',-->
+    <!--                'before:inset-0',-->
+    <!--                'before:bg-blue-500',-->
+    <!--                'before:opacity-20'-->
+    <!--              ]-->
+    <!--            : [-->
+    <!--                'relative',-->
+    <!--                'z-20',-->
+    <!--                'hover:z-0',-->
+    <!--                'before:opacity-0',-->
+    <!--                'border-white',-->
+    <!--                'border-opacity-20'-->
+    <!--              ]-->
+    <!--        "-->
+    <!--      >-->
+    <!--        Бакалавриат-->
+    <!--      </p>-->
+    <!--      <p-->
+    <!--        @click="buttonClick('Специалитет')"-->
+    <!--        class="cursor-pointer border-t border-b py-2 px-2.5 hover:border-fuchsia-400 hover:shadow-[0px_0px_30px_10px_rgba(255,0,255,0.4)] backdrop-blur-2xl transition-all before:absolute before:inset-0 before:bg-fuchsia-500 hover:before:opacity-20"-->
+    <!--        :class="-->
+    <!--          activeTab === 'Специалитет'-->
+    <!--            ? [-->
+    <!--                'z-10',-->
+    <!--                'border-fuchsia-400',-->
+    <!--                'shadow-[0px_0px_30px_10px_rgba(255,0,255,0.4)]',-->
+    <!--                'before:absolute',-->
+    <!--                'before:inset-0',-->
+    <!--                'before:bg-fuchsia-500',-->
+    <!--                'before:opacity-20'-->
+    <!--              ]-->
+    <!--            : [-->
+    <!--                'relative',-->
+    <!--                'z-20',-->
+    <!--                'hover:z-0',-->
+    <!--                'before:opacity-0',-->
+    <!--                'border-white',-->
+    <!--                'border-opacity-20'-->
+    <!--              ]-->
+    <!--        "-->
+    <!--      >-->
+    <!--        Специалитет-->
+    <!--      </p>-->
+    <!--      <p-->
+    <!--        @click="buttonClick('Магистратура')"-->
+    <!--        class="cursor-pointer border-t border-b border-r rounded-r-lg px-4 py-2 hover:border-orange-400 hover:shadow-[0px_0px_30px_10px_rgba(255,150,0,0.4)] backdrop-blur-2xl transition-all before:absolute before:inset-0 before:bg-orange-500 before:rounded-r-lg before:opacity-0 hover:before:opacity-20"-->
+    <!--        :class="-->
+    <!--          activeTab === 'Магистратура'-->
+    <!--            ? [-->
+    <!--                'z-10',-->
+    <!--                'border-orange-400',-->
+    <!--                'shadow-[0px_0px_30px_10px_rgba(255,150,0,0.4)]',-->
+    <!--                'before:absolute',-->
+    <!--                'before:inset-0',-->
+    <!--                'before:bg-orange-500',-->
+    <!--                'before:opacity-20'-->
+    <!--              ]-->
+    <!--            : [-->
+    <!--                'relative',-->
+    <!--                'z-20',-->
+    <!--                'hover:z-0',-->
+    <!--                'before:opacity-0',-->
+    <!--                'border-white',-->
+    <!--                'border-opacity-20'-->
+    <!--              ]-->
+    <!--        "-->
+    <!--      >-->
+    <!--        Магистратура-->
+    <!--      </p>-->
+    <!--    </div>-->
+    <div class="mt-12 text-center rounded-lg bg-white bg-opacity-10 flex flex-col w-1/2 mx-auto">
       <p
         @click="buttonClick(null)"
-        class="cursor-pointer border-t border-b border-l rounded-l-lg px-4 py-2 hover:border-teal-400 hover:shadow-[0px_0px_30px_10px_rgba(0,255,150,0.4)] backdrop-blur-2xl transition-all before:absolute before:inset-0 before:bg-teal-500 before:rounded-l-lg before:opacity-0 hover:before:opacity-20"
+        class="cursor-pointer border-t border-l border-r rounded-t-lg px-4 py-2 hover:border-teal-400 hover:shadow-[0px_0px_30px_10px_rgba(0,255,150,0.4)] backdrop-blur-2xl transition-all before:absolute before:inset-0 before:bg-teal-500 before:rounded-t-lg before:opacity-0 hover:before:opacity-20"
         :class="
           activeTab === null
             ? [
@@ -50,7 +156,7 @@ const buttonClick = (tab: string): void => {
       </p>
       <p
         @click="buttonClick('Бакалавриат')"
-        class="cursor-pointer border-t border-b py-2 px-2.5 hover:border-blue-400 hover:shadow-[0px_0px_30px_10px_rgba(0,0,255,0.4)] backdrop-blur-2xl transition-all before:absolute before:inset-0 before:bg-blue-500 before:opacity-0 hover:before:opacity-20"
+        class="cursor-pointer border-r border-l py-2 px-2.5 hover:border-blue-400 hover:shadow-[0px_0px_30px_10px_rgba(0,0,255,0.4)] backdrop-blur-2xl transition-all before:absolute before:inset-0 before:bg-blue-500 before:opacity-0 hover:before:opacity-20"
         :class="
           activeTab === 'Бакалавриат'
             ? [
@@ -76,7 +182,7 @@ const buttonClick = (tab: string): void => {
       </p>
       <p
         @click="buttonClick('Специалитет')"
-        class="cursor-pointer border-t border-b py-2 px-2.5 hover:border-fuchsia-400 hover:shadow-[0px_0px_30px_10px_rgba(255,0,255,0.4)] backdrop-blur-2xl transition-all before:absolute before:inset-0 before:bg-fuchsia-500 hover:before:opacity-20"
+        class="cursor-pointer border-l border-r py-2 px-2.5 hover:border-fuchsia-400 hover:shadow-[0px_0px_30px_10px_rgba(255,0,255,0.4)] backdrop-blur-2xl transition-all before:absolute before:inset-0 before:bg-fuchsia-500 hover:before:opacity-20"
         :class="
           activeTab === 'Специалитет'
             ? [
@@ -102,7 +208,7 @@ const buttonClick = (tab: string): void => {
       </p>
       <p
         @click="buttonClick('Магистратура')"
-        class="cursor-pointer border-t border-b border-r rounded-r-lg px-4 py-2 hover:border-orange-400 hover:shadow-[0px_0px_30px_10px_rgba(255,150,0,0.4)] backdrop-blur-2xl transition-all before:absolute before:inset-0 before:bg-orange-500 before:rounded-r-lg before:opacity-0 hover:before:opacity-20"
+        class="cursor-pointer border-b border-l border-r rounded-b-lg px-4 py-2 hover:border-orange-400 hover:shadow-[0px_0px_30px_10px_rgba(255,150,0,0.4)] backdrop-blur-2xl transition-all before:absolute before:inset-0 before:bg-orange-500 before:rounded-b-lg before:opacity-0 hover:before:opacity-20"
         :class="
           activeTab === 'Магистратура'
             ? [
