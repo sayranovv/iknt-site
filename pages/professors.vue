5ea9cbae8780356a1e0dc9bcc212e3ec297d67de
Eldar Sayranov <sayranoveldar@gmail.com>
2025-02-24 08:09:43 +0500

    created adaptive layout for professors page

--- pages/professors.vue
+++ pages/professors.vue
@@ -63,14 +63,14 @@ const professors = [
   <div>
     <div class="absolute h-screen w-screen">
       <img
-        src="/bg/bg2_e.jpg"
+        src="/bg/bg2_e.png"
         alt="bg"
         class="absolute -z-10 w-full h-3/5 object-cover object-bottom left-1/2 -translate-x-1/2"
       />
     </div>
-    <div class="max-w-screen-laptop-l mx-auto px-3 tablet:px-5 laptop-l:px-0 pt-52">
-      <h1 class="text-center text-7xl font-bold">Наши преподаватели</h1>
-      <div class="mt-20 grid grid-cols-1 tablet:grid-cols-2 laptop-s:grid-cols-4 gap-x-8 gap-y-16">
+    <div class="max-w-screen-laptop-l mx-auto px-3 tablet:px-5 laptop-l:px-0 pt-40 tablet:pt-52">
+      <h1 class="text-center text-5xl tablet:text-7xl font-bold">Наши преподаватели</h1>
+      <div class="mt-20 grid grid-cols-2 tablet:grid-cols-3 laptop-s:grid-cols-4 gap-x-8 gap-y-16">
         <div v-for="professor in professors" :key="professor.id">
           <img :src="professor.img" alt="professor" class="w-full rounded-lg" />
           <h3 class="font-bold text-2xl leading-none mt-6">{{ professor.name }}</h3>
